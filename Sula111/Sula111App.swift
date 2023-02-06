6e36e8e5099452f31c3b859fbced2bb6230ec46b
test <astappleid@itstep.kz>
2023-02-06 15:43:59 +0600

    initial commit

---
+++ Sula111/Sula111App.swift
@@ -0,0 +1,17 @@
+//
+//  Sula111App.swift
+//  Sula111
+//
+//  Created by swift on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sula111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
